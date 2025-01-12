eb68dd9c817c32be6b9fea3e99a1b87083de2260
Davut Simsek <davutt.simsek@gmail.com>
2025-01-12 17:37:35 +0100

    finished model opening-closing

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+// Variable Declarations
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+console.log(btnsOpenModal);
+
+// Function Declarations
+const openModal = function () {
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+const closeModal = () => {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+// Opening Modal
+for (let btn of btnsOpenModal) {
+  btn.addEventListener(`click`, openModal);
+}
+// Closing Modal
+btnCloseModal.addEventListener(`click`, closeModal);
+overlay.addEventListener(`click`, closeModal);
+// Closing Modal with ESC keyboard
+document.addEventListener(`keydown`, function (e) {
+  e.key === `Escape` && !modal.classList.contains(`hidden`) && closeModal();
+});
